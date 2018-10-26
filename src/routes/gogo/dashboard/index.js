90ad1e38f1412a8b64f14dd776fbf4e5773e801d
Tatiana <tmedeiross@globo.com>
2018-10-26 08:15:41 -0300

    fix

--- src/routes/gogo/dashboard/index.js
+++ src/routes/gogo/dashboard/index.js
@@ -9,87 +9,44 @@ import { BreadcrumbItems } from "Components/BreadcrumbContainer";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Creators as ShopActions } from "Redux/shops/reducer";
-const CustomCard = props => {
-  return (
-    <div>
-      <header
-        style={{
-          borderBottom: "1px solid #eee",
-          paddingBottom: 6,
-          marginBottom: 10,
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          color: props.cardColor
-        }}
-      >
-        <div style={{ fontSize: 14, fontWeight: "bold" }}>{props.name}</div>
-        <div style={{ fontSize: 11 }}>{props.dueOn}</div>
-      </header>
-      <div style={{ fontSize: 12, color: "#BD3B36" }}>
-        <div style={{ color: "#4C4C4C", fontWeight: "bold" }}>
-          {props.subTitle}
-        </div>
-        <div style={{ padding: "5px 0px" }}>
-          <i>{props.body}</i>
-        </div>
-        <div
-          style={{
-            marginTop: 10,
-            textAlign: "center",
-            color: props.cardColor,
-            fontSize: 15,
-            fontWeight: "bold"
-          }}
-        >
-          {props.escalationText}
-        </div>
-      </div>
-    </div>
-  );
-};
+
 const data = {
   lanes: [
     {
       id: "lane1",
       title: "Planned Tasks",
-      label: "2/2",
-      draggable: true,
       cards: [
         {
           id: "Card1",
-          title: "Write Blog",
-          description: "Can AI make memes",
-          label: "30 mins"
+          name: "John Smith",
+          dueOn: "due in a day",
+          subTitle: "SMS received at 12:13pm today",
+          body: "Thanks. Please schedule me for an estimate on Monday.",
+          escalationText: "Escalated to OPS-ESCALATIONS!",
+          cardColor: "#BD3B36",
+          cardStyle: {
+            borderRadius: 6,
+            boxShadow: "0 0 6px 1px #BD3B36",
+            marginBottom: 15
+          }
         },
         {
           id: "Card2",
-          title: "Pay Rent",
-          description: "Transfer via NEFT",
-          label: "5 mins",
-          metadata: { sha: "be312a1" }
+          name: "Card Weathers",
+          dueOn: "due now",
+          subTitle: "Email received at 1:14pm",
+          body: "Is the estimate free, and can someone call me soon?",
+          escalationText: "Escalated to Admin",
+          cardColor: "#E08521",
+          cardStyle: {
+            borderRadius: 6,
+            boxShadow: "0 0 6px 1px #E08521",
+            marginBottom: 15
+          }
         }
       ]
-    },
-    {
-      id: "lane2",
-      title: "Completed",
-      label: "0/0",
-      cards: []
-    },
-    {
-      id: "lane2",
-      title: "Completed",
-      label: "0/0",
-      cards: []
     }
-  ],
-  draggable,
-  onDataChange={onDataChange},
-  handleDragStart={handleDragStart},
-  handleDragEnd={handleDragEnd},
-  handleLaneDragStart={handleLaneDragStart},
-  handleLaneDragEnd={handleLaneDragEnd},
+  ]
 };
 
 export class Dashboard extends Component {
